0b5aef7107db6b9e3a79784dd60419589d029fb2
Frank Rietta <frank@rietta.com>
2013-12-05 14:05:46 -0500

    Playing with voice activation.s

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloVoice::Application.config.secret_key_base = 'acf7646db370426c71532780d6e7cbe79f6e145345ab30929e9fd4720bd951bbe8e8e44c14cdd7bb37d42d272e83a6fa14d8a3ff92099546382bf820a91450c8'
